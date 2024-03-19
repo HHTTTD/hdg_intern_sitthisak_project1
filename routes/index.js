86769d557377f168137c8a8b8d45c7e09da96efb
HHTTTD <sitthisak.srs@spumail.net>
2024-03-19 21:57:42 +0700

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express_Test_API' });
+  res.render('index', { title: 'Express_Test_API#สิทธิศักดิ์' });
 });
 
 module.exports = router;
