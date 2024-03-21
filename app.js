47d6ddc3a947ecb4eb2eee9430a9f2580e358502
HHTTTD <sitthisak.srs@spumail.net>
2024-03-21 20:14:19 +0700

    modified:   app.js 	new file:   public/images/Test.jpg 	new file:   public/images/Test1.gif 	new file:   public/images/background.gif 	new file:   public/images/background1.gif 	modified:   public/stylesheets/style.css 	modified:   views/index.jade

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
